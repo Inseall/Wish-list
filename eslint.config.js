3b05868f4fe0d8889883e7a9238e8e2721e7fb84
Inseall <gamma.case@yandex.ru>
2026-05-16 04:51:42 +0700

    Настройка окружения

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,23 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import globals from 'globals'
+import stylistic from '@stylistic/eslint-plugin'
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-]);
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/semi': ['error', 'always'],
+      'no-console': 'off',
+    },
+  },
+]
